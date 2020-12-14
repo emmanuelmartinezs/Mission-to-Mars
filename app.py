d5e882b5373b3a2006b0f1d8a4dd75bff3cdf45a
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-12-13 20:38:22 -0500

    update

--- app.py
+++ app.py
@@ -15,4 +15,14 @@ def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-   
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+
+if __name__ == "__main__":
+   app.run()   
